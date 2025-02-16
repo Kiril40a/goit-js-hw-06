c3123010104bc209d70d9ec99aeadf660cff0338
Kiril40a <kirilsoroka88@gmail.com>
2025-02-16 14:52:24 +0200

    push

--- js/task-1.js
+++ js/task-1.js
@@ -1,35 +1,28 @@
-
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
+const customer = {  
+  username: "Mango",  
+  balance: 24000,  
+  discount: 0.1,  
+  orders: ["Burger", "Pizza", "Salad"],  
+  getBalance() {  
+    return this.balance;  
+  },  
+  getDiscount() {  
     return this.discount;
-  },
-  setDiscount(value) {
+  },  
+  setDiscount(value) {  
     this.discount = value;
-  },
-  getOrders() {
+  },  
+  getOrders() {  
     return this.orders;
-  },
-  addOrder(cost, order) {
+  },  
+  addOrder(cost, order) {  
     this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
+    this.orders.push(order); 
+  },  
+};  
 
 customer.setDiscount(0.15);
 console.log(customer.getDiscount()); // 0.15
 customer.addOrder(5000, "Steak");
 console.log(customer.getBalance()); // 19750
 console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
-
-
-console.log(``);
-
